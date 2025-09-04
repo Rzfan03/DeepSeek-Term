52a4ce1d49b7ace5ed971ce475b6472cfefcf075
Zennius! <fajrulsyaban47@gmail.com>
2025-09-04 04:17:09 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,74 @@
+from openai import OpenAI
+import pyfiglet
+
+banner = pyfiglet.figlet_format("DeepSeek Term")
+print(banner)
+print(f'DeepSeek in your terminal!')
+print("my github : https://github.com/Rzfan03")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("made by rizfan")
+
+
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key="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",
+)
+
+
+stop_words = ["no"]
+
+print("Hai! Ada yang bisa aku bantu? (Ketik 'no' untuk berhenti)")
+
+
+while True:
+    user_input = input("Anda: ").strip().lower()
+    
+
+    if any(word in user_input for word in stop_words):
+        print("Terima kasih telah menggunakan layanan ini. Sampai jumpa!")
+        break
+    
+
+    if not user_input:
+        print("Silakan masukkan pertanyaan Anda.")
+        continue
+    
+
+    try:
+        completion = client.chat.completions.create(
+            extra_headers={
+                "HTTP-Referer": "https://localhost",
+                "X-Title": "DeepSeek Chat",
+            },
+            extra_body={},
+            model="deepseek/deepseek-r1-distill-llama-70b:free",
+            messages=[
+                {
+                    "role": "user",
+                    "content": user_input
+                }
+            ]
+        )
+        
+        
+        print("DeepSeek: " + completion.choices[0].message.content)
+        print()  
+        
+    except Exception as e:
+        print(f"Error!: {e}")
+        print("Error!")
